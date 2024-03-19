5eb153bab037b35a79dc2b4ebeeddbdab78f0dde
Daniel Lok <daniel.lok@databricks.com>
2024-03-19 12:09:21 +0900

    test
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/tracing/client.py
+++ mlflow/tracing/client.py
@@ -40,7 +40,7 @@ class DummyTraceClientWithHTMLDisplay(TraceClient):
         children: List = field(default_factory=list)
 
     def log_trace(self, trace: Trace):
-        return self._recover_tree(trace)
+        self._root_node = self._recover_tree(trace)
 
     def _recover_tree(self, trace: Trace):
         # Recover the tree from the trace
