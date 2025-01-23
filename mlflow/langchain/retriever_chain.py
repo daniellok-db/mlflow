1554da0287915a36089411f400ed4b7952ad64d6
Daniel Lok <daniel.lok@databricks.com>
2025-01-23 15:20:11 +0800

    ref
    Signed-off-by: Daniel Lok <daniel.lok@databricks.com>

--- mlflow/langchain/retriever_chain.py
+++ mlflow/langchain/retriever_chain.py
@@ -28,7 +28,8 @@ class _RetrieverChain(Chain):
     In order to log the retriever object in the ``langchain`` flavor, the retriever object
     needs to be wrapped within a ``_RetrieverChain``.
 
-    See :ref:`log-retriever-chain` for how to log the ``_RetrieverChain``.
+    See `this guide <../../llms/langchain/guide/#log-retriever` for how to
+    log the ``_RetrieverChain``.
 
     Args:
         retriever: The retriever to wrap.
